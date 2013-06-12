68b7b4d830753af5b4bdf66a2b643ba33561aa5e
Andrew M. C. Dawes <dawes@pacificu.edu>
2013-06-12 00:21:51 -0700

    updated livegraph to have fft example

--- live_graph.py
+++ live_graph.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 
 plt.ion()
 fig = plt.figure()
-#x = np.random.randn(512,512)
-#plt.imshow(abs(np.fft.fftshift(np.fft.fft(x,axis=1),axes=1)))
+x = np.random.randn(16,1024) + np.sin(np.arange(1024))
+ydata = abs(np.fft.fftshift(np.fft.fft(x[8,:])))
+line, = plt.plot(ydata)
 for i in range(50):
-	x = np.random.randn(128,256)
-	plt.imshow(abs(np.fft.fftshift(np.fft.fft(x,axis=1),axes=1)))
-	fig.canvas.draw()
+	x = np.random.randn(16,1024) + np.sin(np.arange(1024))
+	ydata = abs(np.fft.fftshift(np.fft.fft(x[8,:])))
+	line.set_ydata(ydata)
+	plt.draw()
 
 print "done"
